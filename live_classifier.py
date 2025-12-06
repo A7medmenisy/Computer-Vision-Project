1150246c0dd1957ef5a077c26acd5cf88e4d05d3
A7medmenisy <amenesy819@gmail.com>
2025-12-07 01:38:09 +0200

    frist step

---
+++ live_classifier.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:aeb2b71a594b73ebb52a9add1a0b3fdb4002574699c45aa4846af614573d6681
+size 2526
---
+++ traning_code.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:abff45e39bc6f853c38839fd1ec7d846d73ce2716e417728ff202ad1a42dc44e
+size 3106
